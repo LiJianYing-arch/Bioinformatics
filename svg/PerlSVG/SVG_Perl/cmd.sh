7694a27d338c6f182e16584e63ff39f5a740b631
LiJianYing-arch <jyli@mail.hzau.edu.cn>
2021-03-11 17:01:08 +0800

    perl svg plot

---
+++ svg/PerlSVG/SVG_Perl/cmd.sh
@@ -0,0 +1,3 @@
+perl getChrplotSVG-Ga.pl Gagenome_coloredWithGbAtgenome.txt Ga-Gr_len.lst
+
+perl getChrplotSVG-Gr.pl Grgenome_coloredWithGbDtgenome.txt Ga-Gr_len.lst
---
+++ svg/run.sh
@@ -0,0 +1,11 @@
+
+perl SVGplot.pl Pop1_data.txt 0.0000004884914826
+
+
+第一个是用五个文件  
+
+分别是三个基因组染色体长度信息 
+
+
+后面两个文件是共线性信息  每个文件六列
+chr1  Start1 end1   Chr2 start2 end2  
