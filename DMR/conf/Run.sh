e25dd6a7cd2d7f5b7462cf7a1d9c0d45eb77a899
jyli <61865007+LiJianYing-arch@users.noreply.github.com>
2021-03-11 17:24:44 +0800

    Delete DMR/conf directory

--- DMR/conf/Run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash  
-perl ~/software/circos-0.68-pre1/bin/circos -conf circos.conf -param image/radius=1000p
-
-
--- DMR/conf/runMe.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash  
-
-set -o nounset                              # Treat unset variables as an error
-
-/data02/public_software/circos-0.66/bin/circos -conf circos.conf -param image/radius=1000p >log 2>errlog
-
-
